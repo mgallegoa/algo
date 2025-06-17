60e084c27f69fc629990e5446f0f3741cb7a80d2
mgallegoa <manuelgallego2003@yahoo.com>
2025-06-17 18:05:00 -0500

    Algorithm : Max Priority Queue : Method to delete, sink or shift-down a value in maxPQpriority Queue, in java

--- java/src/com/co/manuel/algorithms/priorityQueueBinaryTree/MaxPQ.java
+++ java/src/com/co/manuel/algorithms/priorityQueueBinaryTree/MaxPQ.java
@@ -3,7 +3,8 @@ package com.co.manuel.algorithms.priorityQueueBinaryTree;
 /*
  * Max Priority Queue (Max Heap) is a binary tree in which each node value is >= than the values of its children.
  *
- * Insert or swim a new value in maxPQ
+ * 1. Insert or swim or shift-up a new value in maxPQ (Bottom-up Reheapify)
+ * 2. Delete or sink or shift-down a value in maxPQ (Bottom-down Reheapify)
 */
 public class MaxPQ {
 
@@ -32,7 +33,7 @@ public class MaxPQ {
     swim(n);
   }
 
-  private void resize(int capacity) {
+  protected void resize(int capacity) {
     Integer[] temp = new Integer[capacity];
     for (int i = 0; i < heap.length; i++) {
       temp[i] = heap[i];
@@ -55,6 +56,38 @@ public class MaxPQ {
     }
   }
 
+  public int deleteMax() {
+    int max = heap[1];
+    swap(1, n);
+    n--;
+    sink(1);
+    heap[n + 1] = null;
+    if (n > 0 && (n == (heap.length - 1) / 4)) {
+      resize(heap.length / 2);
+    }
+    return max;
+  }
+
+  public void swap(int a, int b) {
+    int temp = heap[a];
+    heap[a] = heap[b];
+    heap[b] = temp;
+  }
+
+  public void sink(int k) {
+    while (2 * k <= n) {
+      int j = 2 * k;
+      if (j < n && heap[j] < heap[j + 1]) {
+        j++;
+      }
+      if (heap[k] >= heap[j]) {
+        break;
+      }
+      swap(k, j);
+      k = j;
+    }
+  }
+
   public static void main(String[] args) {
     MaxPQ mpq = new MaxPQ(3);
     System.out.println(mpq.size());
@@ -65,7 +98,14 @@ public class MaxPQ {
     mpq.insert(6);
     mpq.insert(1);
     mpq.insert(3);
-    System.out.println(mpq.size());
+    System.out.println("The size of the array is: " + mpq.size());
+    System.out.println("The result of the insert or swim or shift-up data is: ");
+    mpq.printMaxHeap();
+    System.out.println("");
+    System.out.println("");
+    System.out
+        .println("Call to delete or sink or shift-down a max value in maxPQ, the max value was: " + mpq.deleteMax());
+    System.out.println("The result of the delete or sink or shift-down a max value in maxPQ data is: ");
     mpq.printMaxHeap();
   }
 
