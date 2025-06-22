413b92bd0074ccc2eb0ba6cd9d458bd78180c8f9
mgallegoa <manuelgallego2003@yahoo.com>
2025-06-21 21:38:22 -0500

    Algorithm : UndirectionGraphList : Add traversal the Un-directional Graph List using BFS, in java.

--- java/src/com/co/manuel/algorithms/graphs/UndirectionGraphList.java
+++ java/src/com/co/manuel/algorithms/graphs/UndirectionGraphList.java
@@ -1,7 +1,15 @@
 package com.co.manuel.algorithms.graphs;
 
 import java.util.LinkedList;
+import java.util.Queue;
 
+/**
+ * This is a UN Direction Graph using LinkedList.
+ *
+ * The method traversalBFS used the Breadth First Search (BFS) algorithm to
+ * traversal the Graph.
+ *
+ */
 public class UndirectionGraphList {
 
   private LinkedList<Integer>[] adj;
@@ -38,12 +46,34 @@ public class UndirectionGraphList {
     return sb.toString();
   }
 
+  public void traversalBFS(int start) {
+    boolean[] visited = new boolean[this.V];
+
+    Queue<Integer> q = new LinkedList<>();
+    visited[start] = true;
+    q.offer(start);
+
+    while (!q.isEmpty()) {
+      int u = q.poll();
+      System.out.print(u + " ");
+      for (int v : this.adj[u]) {
+        if (!visited[v]) {
+          visited[v] = true;
+          q.offer(v);
+        }
+      }
+    }
+  }
+
   public static void main(String[] args) {
-    UndirectionGraphList ugl = new UndirectionGraphList(4);
+    UndirectionGraphList ugl = new UndirectionGraphList(5);
     ugl.addEdge(0, 1);
     ugl.addEdge(1, 2);
     ugl.addEdge(2, 3);
     ugl.addEdge(3, 0);
+    ugl.addEdge(2, 4);
     System.out.println(ugl);
+    System.out.println("Traverse the UN Directional Graph using Breadth First Search BFS algorithm:");
+    ugl.traversalBFS(0);
   }
 }
