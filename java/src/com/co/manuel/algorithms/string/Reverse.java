38d2862ba0fa53559c06833fd0f15ba0a4c57314
mgallegoa <manuelgallego2003@yahoo.com>
2025-07-15 13:37:00 -0500

    Algorithm : Reverse : Add algorithm to easy reverse string, in java.

--- java/src/com/co/manuel/algorithms/string/Reverse.java
+++ java/src/com/co/manuel/algorithms/string/Reverse.java
@@ -5,6 +5,15 @@ import java.util.Stack;
 public class Reverse {
 
   public String reverse(String initial) {
+    String reverse = "";
+    for (int i = 0; i < initial.length(); i++) {
+      reverse = initial.charAt(i) + reverse;
+    }
+
+    return reverse;
+  }
+
+  public String reverseStack(String initial) {
     char[] chars = initial.toCharArray();
     Stack<Character> reverseStack = new Stack<>();
     for (char character : chars) {
@@ -20,8 +29,8 @@ public class Reverse {
   public static void main(String[] args) {
     String initial = "ABCD";
     System.out.println("Before reverse = " + initial);
-    System.out.println("After reverse = " + new Reverse().reverse(initial));
-
+    System.out.println("After reverse= " + new Reverse().reverse(initial));
+    System.out.println("After reverse using Stack = " + new Reverse().reverseStack(initial));
   }
 
 }
